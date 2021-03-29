6d67d1a31a07b38a95422eef49a1f99d1993fa75
Yonle <63401712+Yonle@users.noreply.github.com>
2021-03-29 10:29:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var refreshCommand = (() => {
 
 if (!BOT_TOKEN) {
 	console.error("[TOKEN] Missing Token.");
-	process.exit(1);
+	return process.exit(1);
 }
 var bot = new Telegraf(BOT_TOKEN);
 console.log("[BOT] Client Loaded.");
